0e240cf4852b5f0394db9445712c484f7563ec34
daniel <danielortizd7mail.com>
2025-02-13 20:49:05 -0500

    Agregada ruta de bienvenida para evitar Cannot GET /

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ conectarDB(); // Conectar a MongoDB
 // Rutas
 app.use('/api/tipoAgua', tipoAguaRoutes);
 
+// Ruta de bienvenida para evitar "Cannot GET /"
+app.get('/', (req, res) => {
+    res.send("Bienvenido a la API de Tipos de Agua 🌊");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
