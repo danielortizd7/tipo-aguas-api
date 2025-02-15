e6ad79ba612a8963b5e90a3d0539260e817980e2
daniel <danielortizd7mail.com>
2025-02-15 01:01:10 -0500

    descripcion otra3

--- app/services/tipoAguaService.js
+++ app/services/tipoAguaService.js
@@ -1,32 +1,15 @@
-const TipoAgua = require('../models/tipoAgua');
+static async registrar(datos) {
+    try {
+        const nuevoTipo = new TipoAgua({
+            tipoDeAgua: datos.tipoDeAgua,
+            tipoPersonalizado: datos.tipoDeAgua === "otra" ? datos.tipoPersonalizado : null,
+            descripcion: datos.descripcion
+        });
 
-class TipoAguaService {
-    static async obtener() {
-        return await TipoAgua.find();
-    }
-
-    static async registrar(datos) {
-        try {
-            const nuevoTipo = new TipoAgua({
-                nombre: datos.nombre,
-                descripcion: datos.descripcion
-            });
-
-            await nuevoTipo.save();
-            return nuevoTipo;
-        } catch (error) {
-            console.error("❌ Error en registrarTipoAgua:", error);
-            throw new Error(`Error al registrar el tipo de agua: ${error.message}`);
-        }
-    }
-
-    static async eliminar(id) {
-        return await TipoAgua.findByIdAndDelete(id);
-    }
-
-    static async actualizar(id, datos) {
-        return await TipoAgua.findByIdAndUpdate(id, datos, { new: true });
+        await nuevoTipo.save();
+        return nuevoTipo;
+    } catch (error) {
+        console.error("❌ Error en registrarTipoAgua:", error);
+        throw new Error(`Error al registrar el tipo de agua: ${error.message}`);
     }
 }
-
-module.exports = TipoAguaService;
